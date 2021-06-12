b1290c885ca2728396ac5c2b0b9fda1698af4144
Olivia Colomier <51862620+oliviacolomier@users.noreply.github.com>
2021-06-12 12:06:34 -0400

    SINGLE JUMP

--- GMTK Jam/Assets/Scripts/PlayerMovement.cs
+++ GMTK Jam/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float MovementSpeed = 1;
     public Rigidbody2D rigidbody2d;
     public float jumpVelocity = 10f;
+    private bool isJumping;
     void Start()
     {
 
@@ -18,13 +19,22 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         var movement = Input.GetAxis("Horizontal");
-        //var verticalMovement = Input.GetAxis("Vertical");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
-        // transform.position += new Vector3(0,verticalMovement,0) * Time.deltaTime * MovementSpeed;
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
-            
+          
             rigidbody2d.velocity = Vector2.up * jumpVelocity;
+            isJumping = true;
+
         }
     }
+
+ void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
 }
