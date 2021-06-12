72269500844d75b67f3e2f80563c94c1c569fd3c
Olivia Colomier <51862620+oliviacolomier@users.noreply.github.com>
2021-06-12 10:11:26 -0400

    added jumping

--- GMTK Jam/Assets/Scripts/PlayerMovement.cs
+++ GMTK Jam/Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,11 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public float MovementSpeed = 1;
+    public Rigidbody2D rigidbody2d;
+    public float jumpVelocity = 10f;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,7 +19,12 @@ public class PlayerMovement : MonoBehaviour
     {
         var movement = Input.GetAxis("Horizontal");
         //var verticalMovement = Input.GetAxis("Vertical");
-        transform.position += new Vector3(movement,0,0) * Time.deltaTime * MovementSpeed; 
-       // transform.position += new Vector3(0,verticalMovement,0) * Time.deltaTime * MovementSpeed;
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        // transform.position += new Vector3(0,verticalMovement,0) * Time.deltaTime * MovementSpeed;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+        }
     }
 }
