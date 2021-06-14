f15358693038f5c63a18c2ba62e539b470a90bf5
Olivia Colomier <51862620+oliviacolomier@users.noreply.github.com>
2021-06-14 13:45:40 -0400

    updated: tutorial, torches, character movement

---
+++ GMTK Jam/Assets/Scripts/TutorialManager2.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager2 : MonoBehaviour
+{
+    public GameObject[] popUps;
+    private int popUpIndex;
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if (i == popUpIndex)
+            {
+                popUps[i].SetActive(true);
+            }
+            else
+            {
+                popUps[i].SetActive(false);
+            }
+        }
+        if (popUpIndex == 0)
+        {
+ 
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                popUpIndex++;
+            }
+
+        }
+        else if (popUpIndex == 1)
+        {
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 2)
+        {
+
+           if (Input.GetKeyDown(KeyCode.G))
+            {
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 3)
+        {
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+                popUpIndex++;
+        }
+        else if (popUpIndex == 4)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+                popUpIndex++;
+        }
+
+
+    }
+}
+
